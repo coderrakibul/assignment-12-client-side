58c240b1cdd212788ddf49b8ff021b71a055b436
unknown <coder.rakibul@gmail.com>
2022-05-22 20:03:26 +0600

    navbar style changed

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
@@ -9,18 +10,16 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 text-primary">
-                        <li><a>Item 1</a></li>
-                        <li><a>Item 1</a></li>
-                        <li><a>Item 3</a></li>
+                        <li><Link to='/home'>Home</Link></li>
+
                     </ul>
                 </div>
-                <a class="btn btn-ghost normal-case text-2xl font-bold">Car Parts</a>
+                <Link to='/' class="btn btn-ghost normal-case text-2xl font-bold">Car Parts</Link>
             </div>
             <div class="navbar-center hidden lg:flex">
                 <ul class="menu menu-horizontal p-0">
-                    <li><a>Item 1</a></li>
-                    <li><a>Item 2</a></li>
-                    <li><a>Item 3</a></li>
+                    <li><Link to='/home'>Home</Link></li>
+
                 </ul>
             </div>
             <div class="navbar-end">
