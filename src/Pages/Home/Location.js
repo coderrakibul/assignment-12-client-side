112de864a22c1b05a1cc880077b431d7e407211d
unknown <coder.rakibul@gmail.com>
2022-05-23 16:37:44 +0600

    location added

--- src/Pages/Home/Location.js
+++ src/Pages/Home/Location.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import StreetLocation from '../../images/location.png';
 
 const Location = () => {
     return (
         <div className='px-12 mt-24'>
             <h2 className='text-3xl font-bold text-center mb-8'>Our Location</h2>
+            <div class="card lg:card-side bg-base-100 shadow-xl">
+                <figure><img src={StreetLocation} alt="Album" /></figure>
+                <div class="card-body flex justify-center items-center">
+                    <h2 class="card-title text-2xl">Address</h2>
+                    <h3 className='text-xl'>State: FL - Florida</h3>
+                    <h3 className='text-xl'>Zip Code: 12086</h3>
+                    <h3 className='text-xl'>City Name: Miami</h3>
+                    <h3 className='text-xl'>Street: 12/7 Mandarin Road</h3>
+                    <h3 className='text-xl'>Shop No: 137, Block-D </h3>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Home/Worker.js
+++ src/Pages/Home/Worker.js
@@ -3,11 +3,15 @@ import React from 'react';
 const Worker = ({ worker }) => {
     const { name, image } = worker;
     return (
-        <div className='flex justify-center'>
+        <div>
+            <div className='flex justify-center'>
             <div class="w-80 h-80 bg-base-100 shadow-xl rounded-full">
                 <figure><img className='rounded-full p-16' src={image} alt="Shoes" /></figure>
-               
             </div>
+        </div>
+        <div>
+            <h3 className='flex justify-center mt-6 text-2xl font-bold'>{name}</h3>
+        </div>
         </div>
     );
 };
