3dd87ddba9ab9eebf0d070985860b9d604f81b66
unknown <coder.rakibul@gmail.com>
2022-05-23 22:29:03 +0600

    my profile added

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -5,15 +5,15 @@ const Dashboard = () => {
     return (
         <div class="drawer drawer-mobile">
             <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-            <div class="drawer-content flex flex-col items-center justify-center">
-                <h2 className='text-3xl'>Dashboard</h2>
+            <div class="drawer-content">
+                <h2 className='text-3xl font-bold'>Dashboard</h2>
                 <Outlet></Outlet>
-                <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+                <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open</label>
 
             </div>
             <div class="drawer-side">
                 <label for="my-drawer-2" class="drawer-overlay"></label>
-                <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+                <ul class="menu p-4 overflow-y-auto w-50 mr-4 bg-base-100 text-base-content">
 
                     <li><Link to="/dashboard">My Orders</Link></li>
                     <li><Link to="/dashboard/addreview">Add a Review</Link></li>
--- src/Pages/Dashboard/MyProfile.js
+++ src/Pages/Dashboard/MyProfile.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+
 
 const MyProfile = () => {
+    const [user] = useAuthState(auth);
+    console.log(user)
+
     return (
         <div>
             <h2>My Profile</h2>
+            <div className='text-center'>
+            {
+                user && <>
+                    <h2 className='text-2xl'>{user.displayName}</h2>
+                    <h3 className='text-xl'>{user.email}</h3>
+                </>
+            }
+            </div>
         </div>
     );
 };
