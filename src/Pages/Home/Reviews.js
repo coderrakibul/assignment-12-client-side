0d992b20615abad072b217f07a68605cca764660
unknown <coder.rakibul@gmail.com>
2022-05-24 13:16:33 +0600

    user reviews added

--- src/Pages/Home/Reviews.js
+++ src/Pages/Home/Reviews.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Review from '../Home/Review';
 
 const Reviews = () => {
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5000/review")
+            .then(res => res.json())
+            .then(data => setReviews(data))
+    }, []);
+
     return (
         <div className='px-12 mt-24'>
             <h2 className='text-3xl font-bold text-center mb-8'>Our Customers Review</h2>
-            <div className='grid sm:grid-cols-1 lg:grid-cols-3 gap-10'>
+            <div className='grid grid-cols-1 gap-10'>
+                {
+                    reviews.map(review => <Review
+                        key={review._id}
+                        review={review}
+                    ></Review>)
+                }
 
             </div>
         </div>
