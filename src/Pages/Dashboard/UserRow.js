cf034ac6b2c6a4bda9a5cc6bd8cea3b7ca940899
unknown <coder.rakibul@gmail.com>
2022-05-24 19:36:20 +0600

    make admin successfull

--- src/Pages/Dashboard/UserRow.js
+++ src/Pages/Dashboard/UserRow.js
@@ -1,13 +1,22 @@
 import React from 'react';
 
-const UserRow = ({user, index}) => {
-    const {email} = user;
+const UserRow = ({ user, index }) => {
+    const { email } = user;
+    const makeAdmin = () => {
+        fetch(`http://localhost:5000/user/admin/${email}`, {
+            method: 'PUT'
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            })
+    }
     return (
         <tr>
             <th>{index + 1}</th>
             <td>{email}</td>
-            <td>Quality Control Specialist</td>
-            <td>Blue</td>
+            <td><button onClick={makeAdmin} class="btn btn-xs">Make Admin</button></td>
+            <td><button class="btn btn-xs text-white bg-red-500">Remove</button></td>
         </tr>
     );
 };
