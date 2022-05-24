de67353b2e1714d5cf4178294fc2c6588f134a9b
unknown <coder.rakibul@gmail.com>
2022-05-24 19:14:27 +0600

    user loaded

--- src/Pages/Shared/Loading.js
+++ src/Pages/Shared/Loading.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Loading = () => {
     return (
-        <div className="flex items-center justify-center ">
+        <div className="flex items-center py-48 justify-center ">
             <div className="w-14 h-14 border-b-4 border-accent rounded-full animate-spin"></div>
         </div>
     );
