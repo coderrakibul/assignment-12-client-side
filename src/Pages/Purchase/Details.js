8d4017333c6f0a38722f03dbb20683f7b9ac8ce1
unknown <coder.rakibul@gmail.com>
2022-05-25 15:07:30 +0600

    product details updated

--- src/Pages/Purchase/Details.js
+++ src/Pages/Purchase/Details.js
@@ -18,9 +18,22 @@ const Details = () => {
     return (
         <div className='px-12 mt-8'>
             <h2 className='text-3xl font-bold text-center mb-8'>Details of the Product</h2>
-            {
-                <h2>{product._id}</h2>
-            }
+            <div>
+                {
+                    <div>
+                        
+                        <div className='flex justify-center'><img src={product.image} alt="" /></div>
+                        <h2 className='text-3xl text-accent'>Product Name: {product.name}</h2>
+                        <h2 className='text-2xl text-accent'>Product Id: {product._id}</h2>
+                        <p>{product.description}</p>
+                        
+                        <h2 className='text-2xl text-accent'>Available Product: {product.quantity}pcs</h2>
+                        <h2 className='text-2xl text-accent'>Minimum Order: {product.order_quantity}pcs</h2>
+                        <h2 className='text-2xl text-accent'>Price: ${product.price}</h2>
+                        
+                    </div>
+                }
+            </div>
         </div>
     );
 };
